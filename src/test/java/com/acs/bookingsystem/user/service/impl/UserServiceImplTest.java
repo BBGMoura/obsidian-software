b8ba60a37fef8e5ac1dc9148cb96083208cb65de
Bruna <brunamoura2000@hotmail.com>
2024-05-04 13:13:38 +0100

    create maven.yml

--- src/test/java/com/acs/bookingsystem/user/service/impl/UserServiceImplTest.java
+++ src/test/java/com/acs/bookingsystem/user/service/impl/UserServiceImplTest.java
@@ -1,6 +1,6 @@
 package com.acs.bookingsystem.user.service.impl;
 
-import com.acs.bookingsystem.booking.exception.NotFoundException;
+import com.acs.bookingsystem.common.exception.NotFoundException;
 import com.acs.bookingsystem.user.dto.UserDTO;
 import com.acs.bookingsystem.user.request.UserRegistrationRequest;
 import com.acs.bookingsystem.user.request.UserUpdateRequest;
