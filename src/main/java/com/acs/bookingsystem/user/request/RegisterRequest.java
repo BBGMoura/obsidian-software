e2e4367b445d8138af4302ea4a55f0440413750a
Bruna <110235610+BBGMoura@users.noreply.github.com>
2025-03-01 18:00:20 +0000

    ABS-8: adding regex for user register (#50)
    Co-authored-by: BBGMoura <brunabgmoura@gmail.com>

--- src/main/java/com/acs/bookingsystem/user/request/RegisterRequest.java
+++ src/main/java/com/acs/bookingsystem/user/request/RegisterRequest.java
@@ -4,9 +4,18 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record RegisterRequest(@NotBlank @Size(max = 50) String firstName,
-                              @NotBlank @Size(max = 50) String lastName,
-                              @Size(max = 254) @Pattern(regexp = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}", message = "Invalid email format") @NotBlank String email,
-                              @Pattern(regexp = "^0\\d{9,10}$", message = "Invalid phone number format (ex. 07112233445)") @NotBlank String phoneNumber,
-                              @NotBlank String password) {
+public record RegisterRequest(@NotBlank @Size(max = 50)
+                              String firstName,
+                              @NotBlank @Size(max = 50)
+                              String lastName,
+                              @Size(max = 254)
+                              @Pattern(regexp = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}",
+                                       message = "Invalid email format") @NotBlank
+                              String email,
+                              @Pattern(regexp = "^0\\d{9,10}$",
+                                       message = "Invalid phone number format (ex. 07112233445)") @NotBlank
+                              String phoneNumber,
+                              @NotBlank @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&+=?'~]).{8,16}$",
+                                      message = "Password must have one special char, one uppercase, one lower case and one number.")
+                              String password) {
 }
